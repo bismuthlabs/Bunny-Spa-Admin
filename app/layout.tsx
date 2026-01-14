9e32a346c89812005f6d893c58d7b8d353e8350b
Bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2026-01-14 21:33:30 +0000

    prep v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/BUNNY-MASSAGE-LOGO.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/BUNNY-MASSAGE-LOGO.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/BUNNY-MASSAGE-LOGO.png",
   },
 }
 
